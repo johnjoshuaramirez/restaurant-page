ae6b6d5988e8ebe70374dcde82aa68a1dde67f88
John Joshua Ramirez <johnjoshuaramirez.social@gmail.com>
2023-03-18 05:11:36 -0800

    style: add food images to menu

--- src/index.js
+++ src/index.js
@@ -1,5 +1,11 @@
 import './style.css';
 import erina from './erina.jpg';
 import ryo from './ryo.jpg';
+import egg from './egg.png';
+import roast from './roast.jpg';
+import transform from './transform.gif';
 
 console.log('hello world!');
+
+// create all functions here
+// replace images with larger sizes

313a3dbe1325fb6b5cde1913c0a5f338d2d63d32
John Joshua Ramirez <johnjoshuaramirez.social@gmail.com>
2023-03-18 05:44:53 -0800

    refactor: create html elements using javascript

--- src/index.js
+++ src/index.js
@@ -5,7 +5,48 @@ import egg from './egg.png';
 import roast from './roast.jpg';
 import transform from './transform.gif';
 
-console.log('hello world!');
+const header = () => {
+   const header = document.createElement('header');
+   header.classList.add('header');
 
-// create all functions here
-// replace images with larger sizes
+   const h1 = document.createElement('h1');
+   h1.classList.add('header__title');
+   h1.innerText = 'Yukihira Restaurant';
+
+   const nav = document.createElement('nav');
+   nav.classList.add('nav');
+
+   const div_1 = document.createElement('div');
+   div_1.classList.add('nav__item');
+   div_1.innerText = 'Home';
+   const div_2 = document.createElement('div');
+   div_2.classList.add('nav__item');
+   div_2.innerText = 'Menu';
+   const div_3 = document.createElement('div')
+   div_3.classList.add('nav__item');
+   div_3.innerText = 'Contact';
+
+   header.appendChild(h1);
+   nav.appendChild(div_1)
+   nav.appendChild(div_2);
+   nav.appendChild(div_3);
+   header.appendChild(nav);
+
+   return header;
+}
+
+const home = () => {
+   const home = document.createElement('main');
+   home.classList.add('container', 'home');
+
+   return home
+}
+
+const app = () => {
+   const app =document.createElement('div');
+   app.appendChild(header());
+   app.appendChild(home());
+   return app
+}
+
+document.getElementById('content').appendChild(app());
